fb37b2acc4dfd75a01cd9bd58b51fa36d54cc91b
Zac Iqbal <zachary.iq@gmail.com>
2018-09-28 01:00:06 -0700

    SoundBank headers are now read and unmarshalled.

--- bnk/bnk.go
+++ bnk/bnk.go
@@ -2,18 +2,47 @@
 package bnk
 
 import (
+	"encoding/binary"
+	"fmt"
 	"io"
+	"math"
 	"os"
+	"strings"
 )
 
+// A File represents an open Wwise SoundBank.
 type File struct {
-	closer io.Closer
+	closer   io.Closer
+	Sections []*SectionHeader
+}
+
+// A SectionHeader represents a single Wwise SoundBank header.
+type SectionHeader struct {
+	Id     [4]byte
+	Length uint32
 }
 
 // NewFile creates a new File for access Wwise SoundBank files. The file is
 // expected to start at position 0 in the io.ReaderAt.
 func NewFile(r io.ReaderAt) (*File, error) {
-	return new(File), nil
+	var bnk File
+
+	sr := io.NewSectionReader(r, 0, math.MaxInt64)
+	for {
+		var hdr SectionHeader
+		err := binary.Read(sr, binary.LittleEndian, &hdr)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+		bnk.Sections = append(bnk.Sections, &hdr)
+
+		sr.Seek(int64(hdr.Length), io.SeekCurrent)
+	}
+
+	return &bnk, nil
 }
 
 // Open opens the File at the specified path using os.Open and prepares it
@@ -44,6 +73,11 @@ func (bnk *File) Close() error {
 	return err
 }
 
-func (*File) String() string {
-	return "File!"
+func (bnk *File) String() string {
+	var b strings.Builder
+	for _, sec := range bnk.Sections {
+		id := string(sec.Id[:len(sec.Id)])
+		fmt.Fprintf(&b, "%s: len(%d)\n", id, sec.Length)
+	}
+	return b.String()
 }
