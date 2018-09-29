a8aca4d9edfcc430b5228ea71c9b35e347b65a2a
Zac Iqbal <zachary.iq@gmail.com>
2018-09-28 23:24:18 -0700

    The DIDX (Data Index) section is now read and unmarshalled.

--- bnk/bnk.go
+++ bnk/bnk.go
@@ -10,43 +10,77 @@ import (
 	"strings"
 )
 
+// The number of bytes used to describe a single data index entry
+// within the DIDX section.
+const DIDX_ENTRY_BYTES = 12
+
+// The identifier for the start of the DIDX (Data Index) section.
+var didxHeaderId = [4]byte{'D', 'I', 'D', 'X'}
+
 // A File represents an open Wwise SoundBank.
 type File struct {
-	closer   io.Closer
-	Sections []*SectionHeader
+	closer       io.Closer
+	IndexSection *DataIndexSection
+	Sections     []*SectionHeader
+}
+
+// A DataIndexSection represents the DIDX section of a SoundBank file.
+type DataIndexSection struct {
+	Header   *SectionHeader
+	WemCount uint32
+	DataMap  map[uint32]WemDescriptor
+}
+
+// A WemDescriptor represents the location of a single wem entity within the
+// SoundBank DATA section.
+type WemDescriptor struct {
+	// The number of bytes from the start of the DATA section's data (after the
+	// header and length) that this wem begins.
+	Offset uint32
+	// The length in bytes of this wem.
+	Length uint32
 }
 
 // A SectionHeader represents a single Wwise SoundBank header.
 type SectionHeader struct {
-	Id     [4]byte
-	Length uint32
+	Identifier [4]byte
+	Length     uint32
 }
 
 // NewFile creates a new File for access Wwise SoundBank files. The file is
 // expected to start at position 0 in the io.ReaderAt.
 func NewFile(r io.ReaderAt) (*File, error) {
-	var bnk File
+	bnk := new(File)
 
 	sr := io.NewSectionReader(r, 0, math.MaxInt64)
 	for {
-		var hdr SectionHeader
-		err := binary.Read(sr, binary.LittleEndian, &hdr)
+		hdr := new(SectionHeader)
+		err := binary.Read(sr, binary.LittleEndian, hdr)
 		if err != nil {
 			if err == io.EOF {
 				break
 			}
 			return nil, err
 		}
-		bnk.Sections = append(bnk.Sections, &hdr)
 
-		sr.Seek(int64(hdr.Length), io.SeekCurrent)
+		switch id := hdr.Identifier; id {
+		case didxHeaderId:
+			sec, err := hdr.NewDataIndexSection(sr)
+			if err != nil {
+				return nil, err
+			}
+			bnk.IndexSection = sec
+		default:
+			bnk.Sections = append(bnk.Sections, hdr)
+			sr.Seek(int64(hdr.Length), io.SeekCurrent)
+		}
 	}
 
-	return &bnk, nil
+	return bnk, nil
 }
 
-// Open opens the File at the specified path using os.Open and prepares it
-// for use as a Wwise SoundBank file.
+// Open opens the File at the specified path using os.Open and prepares it for
+// use as a Wwise SoundBank file.
 func Open(path string) (*File, error) {
 	f, err := os.Open(path)
 	if err != nil {
@@ -74,10 +108,46 @@ func (bnk *File) Close() error {
 }
 
 func (bnk *File) String() string {
-	var b strings.Builder
+	b := new(strings.Builder)
 	for _, sec := range bnk.Sections {
-		id := string(sec.Id[:len(sec.Id)])
-		fmt.Fprintf(&b, "%s: len(%d)\n", id, sec.Length)
+		fmt.Fprintf(b, "%s: len(%d)\n", sec.Identifier, sec.Length)
 	}
+	idx := bnk.IndexSection
+	fmt.Fprintf(b, "%s: len(%d)\n", idx.Header.Identifier, idx.Header.Length)
+	fmt.Fprintf(b, "WEM count: %d\n", idx.WemCount)
+	fmt.Fprintf(b, "WEM IDs: [")
+	total := uint32(0)
+	for wemId, desc := range idx.DataMap {
+		fmt.Fprintf(b, "%d,", wemId)
+		total += desc.Length
+	}
+	fmt.Fprintln(b, "]")
+	fmt.Fprintf(b, "DIDX WEM total size: %d", total)
 	return b.String()
+}
+
+// NewDataIndexSection creates a new DataIndexSection, reading from r, which must
+// be seeked to the start of the DIDX section data.
+// It is an error to call this method on a non-DIDX header.
+func (hdr *SectionHeader) NewDataIndexSection(r io.Reader) (*DataIndexSection, error) {
+	if hdr.Identifier != didxHeaderId {
+		panic(fmt.Sprintf("Expected DIDX header but got: %s", hdr.Identifier))
+	}
+	wemCount := hdr.Length / DIDX_ENTRY_BYTES
+	sec := DataIndexSection{hdr, wemCount, make(map[uint32]WemDescriptor)}
+	for i := uint32(0); i < wemCount; i++ {
+		var wemId uint32
+		err := binary.Read(r, binary.LittleEndian, &wemId)
+		if err != nil {
+			return nil, err
+		}
+		var desc WemDescriptor
+		err = binary.Read(r, binary.LittleEndian, &desc)
+		if err != nil {
+			return nil, err
+		}
+		sec.DataMap[wemId] = desc
+	}
+
+	return &sec, nil
 }
