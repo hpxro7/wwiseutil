11f0bd7f4f97f376bbcf80187d4ca77ece3a9db0
hpxro7 <2935981+hpxro7@users.noreply.github.com>
2018-10-11 01:38:32 -0700

    The File Package data indexes are now marshalled/unmarshalled.

--- pck/file.go
+++ pck/file.go
@@ -9,16 +9,21 @@ import (
 )
 
 import (
+	"github.com/hpxro7/bnkutil/util"
 	"github.com/hpxro7/bnkutil/wwise"
 )
 
 // The number of bytes used to describe the File Package header.
 const HEADER_BYTES = 4 + 4 + 44 + 4
 
+// The number of bytes used to describe a single data index entry.
+const DATA_INDEX_BYTES = 4 + 4 + 4 + 4
+
 // A File represents an open Wwise File Package.
 type File struct {
-	closer io.Closer
-	Header *Header
+	closer  io.Closer
+	Header  *Header
+	Indexes []*DataIndex
 }
 
 // A Header represents a single Wwise File Package header.
@@ -26,7 +31,16 @@ type Header struct {
 	Identifier [4]byte
 	Length     uint32
 	Unknown    [44]byte
-	Count      uint32
+	WemCount   uint32
+}
+
+// A DataIndex represents location and properties of a file within a File
+// Package.
+type DataIndex struct {
+	// The type of data contained at this location.
+	Type uint32
+	// A descriptor of the wem contained at this location, if it is a wem.
+	Descriptor *wwise.WemDescriptor
 }
 
 // NewFile creates a new File for access Wwise File Package files. The file is
@@ -42,6 +56,14 @@ func NewFile(r io.ReaderAt) (*File, error) {
 	}
 	pck.Header = hdr
 
+	for i := uint32(0); i < pck.Header.WemCount; i++ {
+		idx, err := NewDataIndex(sr)
+		if err != nil {
+			return nil, err
+		}
+		pck.Indexes = append(pck.Indexes, idx)
+	}
+
 	return pck, nil
 }
 
@@ -52,6 +74,14 @@ func (pck *File) WriteTo(w io.Writer) (written int64, err error) {
 		return
 	}
 	written = int64(HEADER_BYTES)
+
+	for _, idx := range pck.Indexes {
+		n, err := idx.WriteTo(w)
+		if err != nil {
+			return written, err
+		}
+		written += n
+	}
 	return written, nil
 }
 
@@ -93,4 +123,62 @@ func (pck *File) ReplaceWems(rs ...*wwise.ReplacementWem) {
 
 func (pck *File) DataStart() uint32 {
 	return 0
+}
+
+func NewDataIndex(sr util.ReadSeekerAt) (*DataIndex, error) {
+	var id uint32
+	err := binary.Read(sr, binary.LittleEndian, &id)
+	if err != nil {
+		return nil, err
+	}
+
+	var dataType uint32
+	err = binary.Read(sr, binary.LittleEndian, &dataType)
+	if err != nil {
+		return nil, err
+	}
+
+	var length uint32
+	err = binary.Read(sr, binary.LittleEndian, &length)
+	if err != nil {
+		return nil, err
+	}
+
+	var offset uint32
+	err = binary.Read(sr, binary.LittleEndian, &offset)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DataIndex{dataType, &wwise.WemDescriptor{id, offset, length}}, nil
+}
+
+// WriteTo writes the full contents of this DataIndex to the Writer specified by
+// w.
+func (idx *DataIndex) WriteTo(w io.Writer) (written int64, err error) {
+	err = binary.Write(w, binary.LittleEndian, idx.Descriptor.WemId)
+	if err != nil {
+		return
+	}
+	written = int64(4)
+
+	err = binary.Write(w, binary.LittleEndian, idx.Type)
+	if err != nil {
+		return
+	}
+	written += int64(4)
+
+	err = binary.Write(w, binary.LittleEndian, idx.Descriptor.Length)
+	if err != nil {
+		return
+	}
+	written += int64(4)
+
+	err = binary.Write(w, binary.LittleEndian, idx.Descriptor.Offset)
+	if err != nil {
+		return
+	}
+	written += int64(4)
+
+	return written, nil
 }
