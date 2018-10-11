cc05f4f14536cc60d0d736ba9b407178ac910056
hpxro7 <2935981+hpxro7@users.noreply.github.com>
2018-10-11 01:17:50 -0700

    The File Package header is now marshalled/unmarshalled.

--- pck/file.go
+++ pck/file.go
@@ -2,7 +2,9 @@
 package pck
 
 import (
+	"encoding/binary"
 	"io"
+	"math"
 	"os"
 )
 
@@ -10,21 +12,47 @@ import (
 	"github.com/hpxro7/bnkutil/wwise"
 )
 
+// The number of bytes used to describe the File Package header.
+const HEADER_BYTES = 4 + 4 + 44 + 4
+
 // A File represents an open Wwise File Package.
 type File struct {
 	closer io.Closer
+	Header *Header
+}
+
+// A Header represents a single Wwise File Package header.
+type Header struct {
+	Identifier [4]byte
+	Length     uint32
+	Unknown    [44]byte
+	Count      uint32
 }
 
 // NewFile creates a new File for access Wwise File Package files. The file is
 // expected to start at position 0 in the io.ReaderAt.
 func NewFile(r io.ReaderAt) (*File, error) {
 	pck := new(File)
+	sr := io.NewSectionReader(r, 0, math.MaxInt64)
+
+	hdr := new(Header)
+	err := binary.Read(sr, binary.LittleEndian, hdr)
+	if err != nil {
+		return nil, err
+	}
+	pck.Header = hdr
+
 	return pck, nil
 }
 
 // WriteTo writes the full contents of this File to the Writer specified by w.
 func (pck *File) WriteTo(w io.Writer) (written int64, err error) {
-	return
+	err = binary.Write(w, binary.LittleEndian, pck.Header)
+	if err != nil {
+		return
+	}
+	written = int64(HEADER_BYTES)
+	return written, nil
 }
 
 // Open opens the File at the specified path using os.Open and prepares it for
