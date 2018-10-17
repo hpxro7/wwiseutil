863af203d6e531abcae57d521111698e6c146056
hpxro7 <2935981+hpxro7@users.noreply.github.com>
2018-10-16 22:33:27 -0700

    Added wem replacement tests to the pck package functional tests.

--- pck/functional_test.go
+++ pck/functional_test.go
@@ -3,6 +3,7 @@ package pck
 
 // Large system tests for the bnk package.
 import (
+	"bytes"
 	"os"
 	"path/filepath"
 	"testing"
@@ -16,7 +17,8 @@ import (
 const (
 	testDir = "testdata"
 
-	simpleFilePackage = "simple.pck"
+	simpleFilePackage  = "simple.pck"
+	complexFilePackage = "complex.pck"
 )
 
 func TestSimpleUnchangedFileIsEqual(t *testing.T) {
@@ -56,4 +58,61 @@ func TestUnchangedWriteFileTwiceIsEqual(t *testing.T) {
 		t.Error(err)
 	}
 	wwise.AssertContainerEqualToFile(t, f, pck)
+}
+
+func TestReplaceWemCases(t *testing.T) {
+	util.SkipIfShort(t)
+
+	for _, c := range wwise.ReplacementTestCases {
+		org, err := Open(filepath.Join(testDir, complexFilePackage))
+		if err != nil {
+			t.Error(c.Name+"failed: ", err)
+			t.FailNow()
+		}
+
+		rs := c.Test.Expand(org)
+		failed := assertReplacedFileCorrectness(t, complexFilePackage, rs...)
+		if failed {
+			t.Error("The", c.Name, "test case has failed.\n")
+		}
+	}
+}
+
+func assertReplacedFileCorrectness(t *testing.T, pckPath string,
+	rs ...*wwise.ReplacementWem) (failed bool) {
+	org, err := Open(filepath.Join(testDir, pckPath))
+	if err != nil {
+		t.Error(err)
+		return true
+	}
+
+	replaced, err := Open(filepath.Join(testDir, pckPath))
+	if err != nil {
+		t.Error(err)
+		return true
+	}
+	replaced.ReplaceWems(rs...)
+	reread := rereadFile(t, replaced)
+
+	failed =
+		wwise.AssertReplacementsConsistent(t, org, replaced, reread, rs...)
+	return
+}
+
+func rereadFile(t *testing.T, org *File) *File {
+	orgBytes := new(bytes.Buffer)
+	_, err := org.WriteTo(orgBytes)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	bs := bytes.NewReader(orgBytes.Bytes())
+
+	ctn, err := NewFile(bs)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	return ctn
 }
